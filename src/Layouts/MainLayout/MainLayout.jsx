595e26067fde7c073db83677a809f0c6cb07823a
hasanalam24 <hasanalam365@gmail.com>
2024-06-03 01:25:24 +0600

    footer in the home page

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Shared/Navbar/Navbar";
+import Footer from "../../Shared/Footer/Footer";
 
 
 const MainLayout = () => {
@@ -8,6 +9,7 @@ const MainLayout = () => {
 
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 
 const Contact = () => {
     return (
-        <div className="bg-gray-100">
+        <div className="bg-gray-100 mb-10">
             <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
                 <div className="flex flex-col justify-between">
                     <div className="space-y-2">
