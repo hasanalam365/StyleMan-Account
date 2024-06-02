e80e97c8f4c60a49b25f83b05399094a4a5ac9aa
hasanalam24 <hasanalam365@gmail.com>
2024-06-03 01:21:22 +0600

    contact section in home

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner/Banner";
+import Contact from "../Contact/Contact";
 import Features from "../Features/Features";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Features></Features>
+            <Contact></Contact>
         </div>
     );
 };
