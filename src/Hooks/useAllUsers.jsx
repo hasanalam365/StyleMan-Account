cf874f5f4de55286b8a654046b763a976566f04d
hasanalam24 <hasanalam365@gmail.com>
2024-06-08 23:34:49 +0600

    get error code in client form server

--- src/Hooks/useAllUsers.jsx
+++ src/Hooks/useAllUsers.jsx
@@ -8,8 +8,13 @@ const useAllUsers = () => {
     const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
-            const { data } = await axiosPublic.get("/users")
+            const { data } = await axiosPublic.get('/users', {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('access-token')}`
+                }
+            })
             return data
+            //jwt create korar por headers kora add kora hoyese
         }
     })
 
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,6 +5,30 @@ export const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+
+    axiosSecure.interceptors.request.use(function (config) {
+        // Do something before request is sent
+        const token = localStorage.getItem('access-token')
+        // console.log('request stopped by interceptors', token)
+
+        config.headers.authorization = `Bearer ${token}`
+
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+    //intercepts 401 and 403 status
+    axiosSecure.interceptors.response.use(function (response) {
+
+        return response;
+    }, (error) => {
+
+        console.log('error status in the interceptor', error.response.status)
+        return Promise.reject(error);
+    });
+
     return axiosSecure
 }
 
